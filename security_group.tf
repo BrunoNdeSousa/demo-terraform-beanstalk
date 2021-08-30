2308afa19a5a86687f85c5c0309aa0a38cd149b1
BrunoNdeSousa <bruno.nascimento@osf.digital>
2021-08-30 15:24:04 -0300

    READme update

--- security_group.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_security_group_rule" "example" {
-  description       = "Allow connectivity on port 80"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  security_group_id = "sg-07ba1557dba819a05"
-  cidr_blocks       = ["0.0.0.0/0"]
-  type              = "ingress"
-}
